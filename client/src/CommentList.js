b3791289066ee2bd34c16002012c2fadd4926a30
Felipe Possari <fcpfelipe@hotmail.com>
2022-03-18 15:26:31 -0300

    Update client folder

---
+++ client/src/CommentList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
+
+    switch (comment.status) {
+      case 'approved':
+        content = comment.content;
+        break;
+      case 'pending':
+        content = 'This comment is awaiting moderation';
+        break;
+      default:
+        content = 'This comment has been rejected';
+    }
+
+    return <li key={comment.id}>{content}</li>;
+  });
+  return <ul>{renderedComments}</ul>;
+};
